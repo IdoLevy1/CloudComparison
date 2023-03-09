b53cb4614e782216865f48ea250e4719b8d34dad
Ido Levy <ido657@gmail.com>
2023-03-09 17:16:10 +0200

    add DB

---
+++ DB/Models/CPUModel.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace DB.Models
+{
+    public class CPUModel
+    {
+        public const string AzureTableName = "AzureCloudDB-CPU";
+        public const string GCPTableName = "GoogleCloudDB-CPU";
+        public const string AWSTableName = "AmazonCloudDB-CPU";
+
+        [BsonId]
+        public Guid Id { get; set; }
+        public float Percentage { get; set; }
+        public DateTime TimeStamp { get; set; }
+
+        //MongoHelper db = new MongoHelper("DB");
+        //db.InsertItem(CPUModel.GCPTableName, new CPUModel { Percentage = 100, TimeStamp = DateTime.Now });
+        //db.InsertItem(CPUModel.AWSTableName, new CPUModel { Percentage = 100, TimeStamp = DateTime.Now });
+        //var items = db.LoadItems<CPUModel>(CPUModel.AzureTableName);
+    }
+}
