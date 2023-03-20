e5224ba2ec63638aa080e0753837ffa6fffebf69
Ido Levy <ido657@gmail.com>
2023-03-20 15:37:10 +0200

    Remove App.config files

--- CpuStressTest/Program.cs
+++ CpuStressTest/Program.cs
@@ -21,7 +21,8 @@ namespace NetworkStressTest
             int duration = 60;
             Console.WriteLine("Starting stress test...");
             Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start(); for (int i = 0; i < numThreads; i++)
+            stopwatch.Start(); 
+            for (int i = 0; i < numThreads; i++)
             {
                 int threadNum = i;
                 System.Threading.ThreadPool.QueueUserWorkItem(state =>
@@ -30,7 +31,8 @@ namespace NetworkStressTest
                     Console.WriteLine($"Thread {threadNum + 1} completed.");
                 });
             } while (stopwatch.Elapsed.TotalSeconds < duration) { }
-            stopwatch.Stop(); Console.WriteLine($"Stress test completed in {stopwatch.Elapsed.TotalSeconds} seconds.");
+            stopwatch.Stop();
+            Console.WriteLine($"Stress test completed in {stopwatch.Elapsed.TotalSeconds} seconds.");
             Console.ReadLine();
         }
     }
