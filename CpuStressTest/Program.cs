a834b2323e0335dc64f62651d46d5d4e3ae23b12
Ido Levy <ido657@gmail.com>
2023-03-20 13:45:39 +0200

    Add CpuStressTest.csproj

---
+++ CpuStressTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Diagnostics;
+
+namespace NetworkStressTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                cpuStressTest();
+            }
+        }
+
+        public static void cpuStressTest()
+        {
+            int numThreads = Environment.ProcessorCount;
+            Console.WriteLine($"Number of processor cores: {numThreads}");
+            int duration = 60;
+            Console.WriteLine("Starting stress test...");
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start(); for (int i = 0; i < numThreads; i++)
+            {
+                int threadNum = i;
+                System.Threading.ThreadPool.QueueUserWorkItem(state =>
+                {
+                    while (stopwatch.Elapsed.TotalSeconds < duration) { }
+                    Console.WriteLine($"Thread {threadNum + 1} completed.");
+                });
+            } while (stopwatch.Elapsed.TotalSeconds < duration) { }
+            stopwatch.Stop(); Console.WriteLine($"Stress test completed in {stopwatch.Elapsed.TotalSeconds} seconds.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NetworkStressTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Diagnostics;
+
+namespace NetworkStressTest
+{
+    internal class Program
+    {
+     
+    }
+}
