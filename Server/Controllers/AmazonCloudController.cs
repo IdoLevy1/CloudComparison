c0138d203d022c102174d54e7b4729087821e2e5
Ido Levy <ido657@gmail.com>
2023-04-17 14:00:16 +0300

    Add google and amazon controllers and models

---
+++ Server/Controllers/AmazonCloudController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using Server.Models;
+
+namespace Server.Controllers
+{
+    [Route("AmazonCloud")]
+    [ApiController]
+    public class AmazonCloudController : ControllerBase
+    {
+        [HttpGet("CpuUsage")]
+        public void GetCpuUsage(
+            [FromQuery(Name = "subscriptionId")] string SubscriptionId,
+            [FromQuery(Name = "resourceGroupName")] string ResourceGroupName,
+            [FromQuery(Name = "vmname")] string VirtualMachineName,
+            [FromQuery(Name = "timespan")] string TimeSpan,
+            [FromQuery(Name = "accessToken")] string AccessToken)
+        {
+            //put code here
+        }
+
+        [HttpGet("MemoryUsage")]
+        public void GetMemoryUsage(
+            [FromQuery(Name = "subscriptionId")] string SubscriptionId,
+            [FromQuery(Name = "resourceGroupName")] string ResourceGroupName,
+            [FromQuery(Name = "vmname")] string VirtualMachineName,
+            [FromQuery(Name = "timespan")] string TimeSpan,
+            [FromQuery(Name = "accessToken")] string AccessToken,
+            [FromQuery(Name = "MemorySize")] int MemorySizeInGB)
+        {
+            //put code here
+        }
+
+        [HttpGet("NetworkUsage")]
+        public void GetNetworkUsage(
+            [FromQuery(Name = "subscriptionId")] string SubscriptionId,
+            [FromQuery(Name = "resourceGroupName")] string ResourceGroupName,
+            [FromQuery(Name = "vmname")] string VirtualMachineName,
+            [FromQuery(Name = "timespan")] string TimeSpan,
+            [FromQuery(Name = "accessToken")] string AccessToken)
+        {
+            //put code here
+        }
+
+        [HttpGet("DBCpu")]
+        public string GetCpuDataFromDB()
+        {
+            //return AmazonCloud.GetCpuUsageDataFromDB();
+            return string.Empty;//delete
+        }
+
+        [HttpGet("DBMemory")]
+        public string GetMemoryDataFromDB()
+        {
+            //return AmazonCloud.GetMemoryUsageDataFromDB();
+            return string.Empty;//delete
+        }
+
+        [HttpGet("DBNetwork")]
+        public string GetNetworkDataFromDB()
+        {
+            //return AmazonCloud.GetNetworkUsageDataFromDB();
+            return string.Empty;//delete
+        }
+    }
+}
---
+++ Server/Controllers/GoogleCloudController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [Route("GoogleCloud")]
+    [ApiController]
+    public class GoogleCloudController : ControllerBase
+    {
+        [HttpGet("CpuUsage")]
+        public void GetCpuUsage(
+            [FromQuery(Name = "subscriptionId")] string SubscriptionId,
+            [FromQuery(Name = "resourceGroupName")] string ResourceGroupName,
+            [FromQuery(Name = "vmname")] string VirtualMachineName,
+            [FromQuery(Name = "timespan")] string TimeSpan,
+            [FromQuery(Name = "accessToken")] string AccessToken)
+        {
+            //put code here
+        }
+
+        [HttpGet("MemoryUsage")]
+        public void GetMemoryUsage(
+            [FromQuery(Name = "subscriptionId")] string SubscriptionId,
+            [FromQuery(Name = "resourceGroupName")] string ResourceGroupName,
+            [FromQuery(Name = "vmname")] string VirtualMachineName,
+            [FromQuery(Name = "timespan")] string TimeSpan,
+            [FromQuery(Name = "accessToken")] string AccessToken,
+            [FromQuery(Name = "MemorySize")] int MemorySizeInGB)
+        {
+            //put code here
+        }
+
+        [HttpGet("NetworkUsage")]
+        public void GetNetworkUsage(
+            [FromQuery(Name = "subscriptionId")] string SubscriptionId,
+            [FromQuery(Name = "resourceGroupName")] string ResourceGroupName,
+            [FromQuery(Name = "vmname")] string VirtualMachineName,
+            [FromQuery(Name = "timespan")] string TimeSpan,
+            [FromQuery(Name = "accessToken")] string AccessToken)
+        {
+            //put code here
+        }
+
+        [HttpGet("DBCpu")]
+        public string GetCpuDataFromDB()
+        {
+            //return GoogleCloud.GetCpuUsageDataFromDB();
+            return string.Empty;//delete
+        }
+
+        [HttpGet("DBMemory")]
+        public string GetMemoryDataFromDB()
+        {
+            //return GoogleCloud.GetMemoryUsageDataFromDB();
+            return string.Empty;//delete
+        }
+
+        [HttpGet("DBNetwork")]
+        public string GetNetworkDataFromDB()
+        {
+            //return GoogleCloud.GetNetworkUsageDataFromDB();
+            return string.Empty;//delete
+        }
+    }
+}
