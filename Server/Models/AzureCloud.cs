b27cb5ef9648d7e00a625a2c23ec8d145e96875b
Ido Levy <ido657@gmail.com>
2023-04-29 11:30:38 +0300

    change syntax

--- Server/Models/AzureCloud.cs
+++ Server/Models/AzureCloud.cs
@@ -9,50 +9,9 @@ namespace Server.Models
 {
     public class AzureCloud
     {
+        private const string AzureCloudName = "AzureCloud";
         private static readonly MongoHelper DB = new MongoHelper("DB");
-        public const string AzureCloudName = "AzureCloud";
-        public static string BuildUrl(string SubscriptionId, string ResourceGroupName, string VirtualMachineName, string TimeSpan, string MetricName)
-        {
-            var url = $"https://management.azure.com/subscriptions/{SubscriptionId}/resourceGroups/{ResourceGroupName}/providers/Microsoft.Compute/virtualMachines/{VirtualMachineName}/providers/microsoft.insights/metrics";
-            url = QueryHelpers.AddQueryString(url, new Dictionary<string, string?>
-            {
-                { "api-version", "2018-01-01" },
-                { "metricnames", MetricName },
-                { "timespan", TimeSpan }
-            });
 
-            return url ;
-        }
-
-        private static dynamic GetInfoFromResponse(RestResponse Response)
-        {
-            if(Response.StatusCode == System.Net.HttpStatusCode.OK)
-            {
-                dynamic info = JsonConvert.DeserializeObject(Response.Content);
-                return info?.value[0].timeseries[0].data[0];
-            }
-            else
-            {
-                throw new Exception("Can't get info from VM");
-            }
-        }
-        
-        public static dynamic GetMetricInfo(string SubscriptionId,
-            string ResourceGroupName,
-            string VirtualMachineName,
-            string TimeSpan,
-            string AccessToken,
-            string MetricName)
-        {
-            var url = BuildUrl(SubscriptionId, ResourceGroupName, VirtualMachineName, TimeSpan, MetricName);
-            var options = new RestClientOptions(url) { MaxTimeout = -1 };
-            var client = new RestClient(options);
-            var request = new RestRequest();
-            request.AddHeader("Authorization", AccessToken);
-            RestResponse response = client.Execute(request);
-            var info = GetInfoFromResponse(response);
-            return info;
-        }
         public static void InsertInfoToDB(
             string SubscriptionId,
             string ResourceGroupName,
@@ -86,25 +45,68 @@ namespace Server.Models
             return JsonConvert.SerializeObject(items);
         }
 
-        public static double GetCpuUsageInfo(string SubscriptionId, string ResourceGroupName, string VirtualMachineName, string TimeSpan, string AccessToken)
+        private static string BuildUrl(string SubscriptionId, string ResourceGroupName, string VirtualMachineName, string TimeSpan, string MetricName)
+        {
+            var url = $"https://management.azure.com/subscriptions/{SubscriptionId}/resourceGroups/{ResourceGroupName}/providers/Microsoft.Compute/virtualMachines/{VirtualMachineName}/providers/microsoft.insights/metrics";
+            url = QueryHelpers.AddQueryString(url, new Dictionary<string, string?>
+            {
+                { "api-version", "2018-01-01" },
+                { "metricnames", MetricName },
+                { "timespan", TimeSpan }
+            });
+
+            return url ;
+        }
+
+        private static dynamic GetInfoFromResponse(RestResponse Response)
+        {
+            if(Response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                dynamic info = JsonConvert.DeserializeObject(Response.Content);
+                return info?.value[0].timeseries[0].data[0];
+            }
+            else
+            {
+                throw new Exception("Can't get info from VM");
+            }
+        }
+        
+        private static dynamic GetMetricInfo(string SubscriptionId,
+            string ResourceGroupName,
+            string VirtualMachineName,
+            string TimeSpan,
+            string AccessToken,
+            string MetricName)
+        {
+            var url = BuildUrl(SubscriptionId, ResourceGroupName, VirtualMachineName, TimeSpan, MetricName);
+            var options = new RestClientOptions(url) { MaxTimeout = -1 };
+            var client = new RestClient(options);
+            var request = new RestRequest();
+            request.AddHeader("Authorization", AccessToken);
+            RestResponse response = client.Execute(request);
+            var info = GetInfoFromResponse(response);
+            return info;
+        }
+
+        private static double GetCpuUsageInfo(string SubscriptionId, string ResourceGroupName, string VirtualMachineName, string TimeSpan, string AccessToken)
         {
             var info = GetMetricInfo(SubscriptionId, ResourceGroupName, VirtualMachineName, TimeSpan, AccessToken, "Percentage%20CPU");
             return info.average;
         }
 
-        public static double GetMemoryUsageInfo(string SubscriptionId, string ResourceGroupName, string VirtualMachineName, string TimeSpan, string AccessToken, double MemorySizeInGB)
+        private static double GetMemoryUsageInfo(string SubscriptionId, string ResourceGroupName, string VirtualMachineName, string TimeSpan, string AccessToken, double MemorySizeInGB)
         {
             var info = GetMetricInfo(SubscriptionId, ResourceGroupName, VirtualMachineName, TimeSpan, AccessToken, "Available Memory Bytes");
             return (info.average * 100) / (double)(MemorySizeInGB * Math.Pow(2, 30));
         }
 
-        public static double GetNetworkInUsageInfo(string SubscriptionId, string ResourceGroupName, string VirtualMachineName, string TimeSpan, string AccessToken)
+        private static double GetNetworkInUsageInfo(string SubscriptionId, string ResourceGroupName, string VirtualMachineName, string TimeSpan, string AccessToken)
         {
             var info = GetMetricInfo(SubscriptionId, ResourceGroupName, VirtualMachineName, TimeSpan, AccessToken, "Network In");
             return (info.total * 8) / Math.Pow(2, 20); // from bytes/sec to Mbits/sec
         }
 
-        public static double GetNetworkOutUsageInfo(string SubscriptionId, string ResourceGroupName, string VirtualMachineName, string TimeSpan, string AccessToken)
+        private static double GetNetworkOutUsageInfo(string SubscriptionId, string ResourceGroupName, string VirtualMachineName, string TimeSpan, string AccessToken)
         {
             var info = GetMetricInfo(SubscriptionId, ResourceGroupName, VirtualMachineName, TimeSpan, AccessToken, "Network Out");
             return (info.total * 8) / Math.Pow(2, 20); // from bytes/sec to Mbits/sec
