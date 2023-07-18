6ff18db5c2c7d2716de01eead01c058af1365e54
Ido Levy <100139604+IdoLevy1@users.noreply.github.com>
2023-07-18 19:09:11 +0300

    Add endpoint of Metrics result to renk the cloud providers

---
+++ Server/Controllers/MetricsResultsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Server.Models;
+
+namespace Server.Controllers
+{
+    [Route("MetricsResults")]
+    [ApiController]
+    public class MetricsResultsController : ControllerBase
+    {
+
+        [HttpGet("GetMetrics")]
+        public ActionResult GetInfoFromDB()
+        {
+            try
+            {
+                MetricsResults results = new MetricsResults();
+                return Ok(results.GetMetricsResultsFromDB());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
