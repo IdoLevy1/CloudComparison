5d741eb3e21e02b5a5bba2f5f6ae0baa4389dc45
Chen Landau <chenla@mta.ac.il>
2023-08-05 13:58:27 +0300

    change TimeSelectionMenu

--- client/client/src/components/TimeSelection.js
+++ client/client/src/components/TimeSelection.js
@@ -4,18 +4,21 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "../styles/TimeSelection.css";
 
-const TimeSelection = ({ onSelectChange, onDateChange, isRealTime }) => {
+const TimeSelection = ({ onSelectChange, onDateChange, isRealTime, isCustom }) => {
   const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [selectedValue, setSelectedValue] = useState('real-time');
 
   const handleSelectChange = (event) => {
     const value = event.target.value;
+    setSelectedValue(value);
     onSelectChange(value);
   };
 
   const handleSubmit = () => {
     console.log(startDate);
-    if (startDate) {
-      onDateChange(startDate);
+    if (startDate && endDate) { // add endDate > StartDate chec
+      onDateChange(startDate,endDate);
     }
   };
 
@@ -31,20 +34,35 @@ const TimeSelection = ({ onSelectChange, onDateChange, isRealTime }) => {
           fontSize: "18px",
           fontFamily: "Tahoma, Verdana, Segoe, sans-serif",
         }}
-        value={isRealTime ? "real-time" : "history"}
+        value={selectedValue}
         onChange={handleSelectChange}
         className="selectState"
       >
         <MenuItem value="real-time">Real-time</MenuItem>
-        <MenuItem value="history">History</MenuItem>
+        <MenuItem value="Last-Week">Last Week</MenuItem>
+        <MenuItem value="Last-Month">Last Month</MenuItem>
+        <MenuItem value="Custom">Custom</MenuItem>
       </Select>
-      {!isRealTime && (
+      {!isRealTime && isCustom && (
         <div className="date-picker-container">
           <div className="date-picker-wrapper">
             <label>Start Date:</label>
             <DatePicker
               selected={startDate}
               onChange={(date) => setStartDate(date)}
+              showTimeSelect
+              timeFormat="HH:mm"
+              timeIntervals={15}
+              timeCaption="Time"
+              dateFormat="yyyy-MM-dd HH:mm"
+              minDate={new Date(2023, 4, 15)}
+              maxDate={new Date(2023, 6, 30)}
+              className="date-picker"
+            />
+            <label>End Date:</label>
+            <DatePicker
+              selected={endDate}
+              onChange={(date) => setEndDate(date)}
               showTimeSelect
               timeFormat="HH:mm"
               timeIntervals={15}
